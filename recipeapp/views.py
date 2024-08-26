c582c0ab39f3bb1d477aa765f26c44607065cf3c
hham90 <148152147+hham90@users.noreply.github.com>
2024-08-26 01:18:19 -0400

    replaced more recipes

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -23,7 +23,7 @@ def login_view(request):
            if user is not None:                    #if user is authenticated
           #then use pre-defined Django function to login
                login(request, user)
-               return redirect('recipes:list')
+               return redirect('Recipes:list')
        else:
            error_message ='ooops.. something went wrong'   #print error message
 
